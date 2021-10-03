deea9949038cecd128fd756feb16c0eecd0ff851
Sam Davenport <samueldavenport@att.net>
2021-10-03 07:35:15 -0500

    added functions to generate each type of random charactor

--- assets/js/script.js
+++ assets/js/script.js
@@ -21,6 +21,16 @@ generateBtn.addEventListener("click", writePassword);
 // Character getting functions
 
 var getLower = function() {
-    return String.fromCharCode(Math.floor(Math.random * 26) + 97);
+    return String.fromCharCode(Math.floor(Math.random() * 26) + 97);
 }
-console.log(getLower());
+var getUpper = function() {
+    return String.fromCharCode(Math.floor(Math.random() * 26) + 65);
+}
+var getNumeric = function() {
+    return String.fromCharCode(Math.floor(Math.random() * 10) + 48);
+}
+var getSpecial = function() {
+    const special = "!@#$%^&*()_-+="
+    return special[Math.floor(Math.random() * special.length)];
+}
+console.log(getSpecial());
